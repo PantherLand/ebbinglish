a9f48eba2c6d22024fd24db93d8bde0f0d79f743
B3TcH0qlSJ4L <B3TcH0qlSJ4L@gmail.com>
2026-03-02 12:23:48 +0800

    feat: wrap TopProgressController in Suspense for improved loading handling
    - Added Suspense around TopProgressController to manage loading states more effectively.
    - This change enhances the user experience by providing a fallback during component loading.

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { Suspense } from "react";
 import TopProgressController from "@/app/components/top-progress-controller";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
-      <TopProgressController />
+      <Suspense fallback={null}>
+        <TopProgressController />
+      </Suspense>
     </>
   );
 }
